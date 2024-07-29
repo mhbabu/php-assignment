5a4cb653cadf9c267689f653d66b9f9183d581e1
mhbabu <write2babu@gmail.com>
2024-07-29 17:05:03 +0600

    Param wise buyers filltered done

--- classes/Application.php
+++ classes/Application.php
@@ -4,10 +4,6 @@ require_once('DB_Connection.php');
 
 class Application extends DB_Connection
 {
-	protected $startDate;
-    protected $endDate;
-	protected $userId;
-
 	public function __construct(){
 		parent::__construct();
 	}
@@ -20,8 +16,32 @@ class Application extends DB_Connection
 	}
 
 	public function getBuyersByFiltering($params){
-		print_r($params);
-		exit();
+
+		if (empty(array_filter($params))) {
+			return ['status' => 'error', 'message' => 'Please select at least one item.'];
+		}
+
+		$sql = "SELECT * FROM buyers";
+
+		if(isset($params['start_date']) && isset($params['end_date'])){
+			$sql  .= " WHERE entry_at BETWEEN '$params[start_date]' AND '$params[end_date]'";
+		}else{
+			if(isset($params['start_date'])){
+				$sql  .= " WHERE entry_at >= '$params[start_date]'";
+			}
+			if(isset($params['end_date'])){
+				$sql  .= " WHERE entry_at <= '$params[end_date]'";
+			}
+		}
+
+		$sql .= " ORDER BY id DESC";
+
+		if (mysqli_query($this->db_connect, $sql)) {
+			$result = mysqli_query($this->db_connect, $sql);
+			return ['data' =>  $result, 'params' => $params];
+		}else{
+			return ['status' => 'error', 'message' =>  $this->db_connect->error];
+		}
 		
 	}
 }
--- pages/filter/buyer-filter-page.php
+++ pages/filter/buyer-filter-page.php
@@ -1,10 +1,20 @@
-<?php 
-    if(isset($_GET['filter_buyer'])){
-        $filteringData = $application->getBuyersByFiltering($_GET); 
+<?php
+    if (isset($_GET['filter_buyer'])) {
+        $result = $application->getBuyersByFiltering($_GET);
     }
 ?>
 
 <link rel="stylesheet" href="../../assets/css/flatpickr.min.css">
+<?php if(isset($result) && $result['status'] === 'error'){ ?>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Error!</strong> <?php echo $result['message']; ?>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        </div>
+    </div>
+<?php } ?>
 <div class="card">
     <div class="card-header d-flex justify-content-between align-items-center">
         <h5 class="card-title fw-bold mb-0">
@@ -18,40 +28,92 @@
     <form method="GET">
         <div class="card-body">
             <div class="row">
-
-                <div class="mb-3 col-md-4">
-                    <label for="userId" class="form-label">User (We will use EntryBy as UserID)</label>
-                    <input type="text" class="form-control" id="userId" name="user_id" placeholder="Enter a single digit 1 to 5 or others">
-                </div>
-
                 <div class="mb-3 col-md-4">
                     <label for="startDate" class="form-label">Start Date</label>
-                    <input type="text" class="form-control" id="startDate" name="start_date" autocomplete="off" placeholder="YYYY-MM-DD">
+                    <input type="text" class="form-control" id="startDate" name="start_date" value="<?php echo isset($result['params']['start_date']) ? $result['params']['start_date'] : null; ?>" autocomplete="off" placeholder="YYYY-MM-DD">
                 </div>
 
                 <div class="mb-3 col-md-4">
                     <label for="endDate" class="form-label">End Date</label>
-                    <input type="text" class="form-control date-picker" id="endDate" name="end_date" autocomplete="off" placeholder="YYYY-MM-DD">
+                    <input type="text" class="form-control date-picker" id="endDate" name="end_date" value="<?php echo isset($result['params']['end_date']) ? $result['params']['end_date'] : null; ?>" autocomplete="off" placeholder="YYYY-MM-DD">
                 </div>
 
             </div>
         </div>
         <div class="card-footer">
-            <div class="d-flex justify-content-between align-items-center">
-                <a class="btn btn-danger btn-sm fw-bold" href="../../buyer_filter.php">
+            <?php if(isset($result['params'])){?>
+                <a class="btn btn-danger btn-sm fw-bold float-start" href="../../buyer_filter.php">
                     <i class="fa fa-undo"></i> Reset
                 </a>
-                <button type="submit" name="filter_buyer" class="btn btn-primary btn-sm">
-                    <i class="fa fa-filter"></i> Filter
-                </button>
-            </div>
+            <?php } ?>
+            
+            <button type="submit" name="filter_buyer" class="btn btn-primary btn-sm float-end">
+                <i class="fa fa-filter"></i> Filter
+            </button>
+             <div class="clearfix"></div>
         </div>
     </form>
 </div>
+<?php if(isset($result['data'])){  ?>
+    <div class="card mt-5 mb-5">
+        <div class="card-header d-flex justify-content-between align-items-center">
+            <h5 class="card-title fw-bold mb-0">
+                <i class="fa fa-users"></i> Buyers Filtered List
+            </h5>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">SL.</th>
+                                <th scope="col">Buyer</th>
+                                <th scope="col">Amount</th>
+                                <th scope="col">Receipt ID</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">IP Address</th>
+
+                                <th scope="col">City</th>
+                                <th scope="col">Phone</th>
+                                <th scope="col">Entry At</th>
+                                <th scope="col">Entry By</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php if ($result['data']->num_rows > 0) { ?>
+                                <?php foreach ($result['data'] as $index => $buyer) { ?>
+                                    <tr>
+                                        <th scope="row"><?php echo ++$index; ?> .</th>
+                                        <td><?php echo $buyer['buyer']; ?></td>
+                                        <td><?php echo $buyer['amount']; ?></td>
+                                        <td><?php echo $buyer['receipt_id']; ?></td>
+                                        <td><?php echo $buyer['buyer_email']; ?></td>
+                                        <td><?php echo $buyer['buyer_ip']; ?></td>
+                                        <td><?php echo $buyer['city']; ?></td>
+                                        <td><?php echo $buyer['phone']; ?></td>
+                                        <td><?php echo $buyer['entry_at']; ?></td>
+                                        <td><?php echo $buyer['entry_by']; ?></td>
+                                    </tr>
+                                <?php } ?>
+                            <?php } else { ?>
+                                <tr class="text-center">
+                                    <td colspan="10">No record found...!</td>
+                                </tr>
+                            <?php } ?>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php } ?>
+
 <script src="../../assets/js/jquery-3.6.0.min.js"></script>
 <script src="../../assets/js/flatpickr.js"></script>
 <script>
-   $(document).ready(function() {
+    $(document).ready(function() {
         $("#startDate, #endDate").flatpickr({
             maxDate: new Date(),
             dateFormat: "Y-m-d"
